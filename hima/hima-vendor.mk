deb056a5a7edfbc17b85bd13d06d8a9ca3200a8a
Joel Stein <js@funkturm.net>
2019-10-04 00:39:21 +0200

    hima: add missing liboemcrypto.so
    Change-Id: If4381822ab1fc54d649f4a457e426a33758b7ca8

--- hima/hima-vendor.mk
+++ hima/hima-vendor.mk
@@ -882,7 +882,8 @@ PRODUCT_COPY_FILES += \
     vendor/htc/hima/proprietary/vendor/lib64/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib64/mediadrm/libwvdrmengine.so \
     vendor/htc/hima/proprietary/vendor/bin/hw/android.hardware.drm@1.0-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.0-service.widevine \
     vendor/htc/hima/proprietary/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.0-service.widevine.rc \
-    vendor/htc/hima/proprietary/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
+    vendor/htc/hima/proprietary/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so \
+    vendor/htc/hima/proprietary/vendor/lib/liboemcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboemcrypto.so
 
 PRODUCT_PACKAGES += \
     libmm-disp-apis \
